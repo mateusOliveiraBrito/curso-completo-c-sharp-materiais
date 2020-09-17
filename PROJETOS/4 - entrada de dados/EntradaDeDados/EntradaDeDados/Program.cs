8c7bb65d4514a6806e77b00b906793441e9bd731
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-17 18:34:48 -0300

    :art: Adiciona projeto da primeira parte sobre entrada de dados

---
+++ PROJETOS/4 - entrada de dados/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EntradaDeDados {
+    class Program {
+        static void Main(string[] args) {
+            string frase, x, y, z, a, b, c;
+
+            frase = Console.ReadLine();
+            x = Console.ReadLine();
+            y = Console.ReadLine(); ;
+            z = Console.ReadLine();
+
+            char[] condicoes = {' ', '.', ';'};
+
+            string[] vetor = Console.ReadLine().Split(condicoes);
+            a = vetor[0];
+            b = vetor[1];
+            c = vetor[2];
+
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine("{0} {1} {2}", a, b, c);
+        }
+    }
+}

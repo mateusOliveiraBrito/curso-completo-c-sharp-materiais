502ca3145c083dfb7b4b744876ab32749e9ac489
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 11:00:58 -0300

    :art: Renomeia solução; Implementa Exercício 02 da lista de exercícios propostos sobre entrada de dados

---
+++ PROJETOS/5 - exercicios propostos/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o valor do raio de um círculo:");
+            double raio = double.Parse(Console.ReadLine());
+
+            double area = 3.14159 * (Math.Pow(raio, 2));
+
+            Console.WriteLine("A = {0}", area.ToString("F4"));
+        }
+    }
+}

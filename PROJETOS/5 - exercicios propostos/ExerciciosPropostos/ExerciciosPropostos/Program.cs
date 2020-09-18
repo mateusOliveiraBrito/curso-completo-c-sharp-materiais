c8b28bdc26fce44ca92290a697d34967414f16ef
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 10:51:56 -0300

    :art: Implementa exercicio 1 da lista de exercícios propostos

---
+++ PROJETOS/5 - exercicios propostos/ExerciciosPropostos/ExerciciosPropostos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExerciciosPropostos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o primeiro valor inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor inteiro:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int soma = n1 + n2;
+
+            Console.WriteLine("SOMA = {0}", soma);
+        }
+    }
+}

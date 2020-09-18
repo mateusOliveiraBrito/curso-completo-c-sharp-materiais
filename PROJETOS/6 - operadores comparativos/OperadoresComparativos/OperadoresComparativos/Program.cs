304f2a2726a1a5f7ca2d9605551b4539ac2d8f85
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 12:14:46 -0300

    :art: Adiciona projeto referente a operadores comparativos;

---
+++ PROJETOS/6 - operadores comparativos/OperadoresComparativos/OperadoresComparativos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperadoresComparativos {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            bool c1 = a > 10;
+            bool c2 = a >= 10;
+            bool c3 = a < 10;
+            bool c4 = a <= 10;
+            bool c5 = a == 10;
+            bool c6 = a != 10;
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+        }
+    }
+}

7edaa064cb7954ea4976971914cd568e8cbdf4e1
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 14:19:28 -0300

    :art: Adiciona projeto referente à aula de operadores lógicos

---
+++ PROJETOS/7 - operadores logicos/OpLogicos/OpLogicos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OpLogicos {
+    class Program {
+        static void Main(string[] args) {
+            bool c1 = 2 > 3 && 4 != 5;
+            bool c2 = 2 > 3 || 4 != 5; //true
+            bool c3 = !(2 > 3) && 4 != 5; //true
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+
+            //PRECEDÊNCIA
+            Console.WriteLine("=======");
+            bool c4 = 10 < 5; //false
+            bool c5 = c2 || c3 && c4;
+            Console.WriteLine(c5);
+
+        }
+    }
+}

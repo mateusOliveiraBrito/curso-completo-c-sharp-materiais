8a59b1ec89484718c268c318d6ba5313a03b44bf
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 11:48:48 -0300

    :art: Implementa Exercicio05 da lista de exercicios propostos sobre estrutura sequencial

---
+++ PROJETOS/5 - exercicios propostos/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio05 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o código da 1° peça: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de peças : ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor unitário de cada peça: ");
+            double valorUnitario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o código da 2° peça: ");
+            int codigo2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de peças : ");
+            int quantidade2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor unitário de cada peça: ");
+            double valorUnitario2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorTotal = (quantidade * valorUnitario) + (quantidade2 * valorUnitario2);
+
+            Console.WriteLine("VALOR A PAGAR: R$ {0}", valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

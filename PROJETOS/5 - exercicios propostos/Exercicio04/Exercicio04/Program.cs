ff1a5c3f5061685f56bc9a2791fe326bcb51b6a6
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 11:27:18 -0300

    :art: Implementa o Exercicio 04 da lista de Exercicios propostos sobre entrada de dados e estrutura sequencial

---
+++ PROJETOS/5 - exercicios propostos/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio04 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite seu número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número de horas trabalhadas: ");
+            int horas = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor recebido por hora: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = horas * valor;
+
+            Console.WriteLine("NUMBER = {0}", numero);
+            Console.WriteLine("SALARY = U$ {0}", salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

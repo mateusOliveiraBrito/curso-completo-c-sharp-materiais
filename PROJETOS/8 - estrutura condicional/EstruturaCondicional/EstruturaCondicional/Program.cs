734a1dfb9b3722d1b49841f3dd0bea968fd10f40
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 18:16:06 -0300

    :art: Adiciona projeto referente ao conteúdo de estrutura condicional

---
+++ PROJETOS/8 - estrutura condicional/EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EstruturaCondicional {
+    class Program {
+        static void Main(string[] args) {
+            //PRIMEIRO EXEMPLO
+            /*int x = 10;
+
+            Console.WriteLine("Bom dia");
+
+            if (x > 5) {
+                Console.WriteLine("Boa tarde");
+            }
+
+            Console.WriteLine("Boa noite");*/
+
+            //SEGUNDO EXEMPLO
+            /*Console.Write("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0) {
+                Console.WriteLine($"{n} é par!");
+            }
+            else {
+                Console.WriteLine($"{n} é ímpar!");
+            }*/
+
+            //TERCEIRO EXEMPLO
+            Console.Write("Qual a hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) 
+                Console.WriteLine("Bom dia!");
+             else if (hora < 18) 
+                Console.WriteLine("Boa tarde!");
+            else if (hora >= 18) 
+                Console.WriteLine("Boa noite!");
+        }
+    }
+}

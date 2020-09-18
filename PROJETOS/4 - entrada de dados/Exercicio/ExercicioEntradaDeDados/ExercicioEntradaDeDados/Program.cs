127d69fa897b42c1e31fde5a24943e69a70a8698
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 10:34:24 -0300

    :art: Adiciona projeto referente ao Exercício proposto sobre Entrada de Dados em C#

---
+++ PROJETOS/4 - entrada de dados/Exercicio/ExercicioEntradaDeDados/ExercicioEntradaDeDados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercicioEntradaDeDados {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nomeComepleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int nQuartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
+            string[] dados = Console.ReadLine().Split(' ');
+
+            string ultimoNome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2]);
+
+            string saida = $@"{nomeComepleto}
+                          {nQuartos}
+                          {preco.ToString("F2")}
+                          {ultimoNome}
+                          {idade}
+                          {altura.ToString("F2")}";
+
+            Console.WriteLine(saida);
+        }
+    }
+}

b64640a7331ad1bc9bac7c5e91bf66d1d02ff823
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-18 11:07:26 -0300

    :art: Implementa Exercicio03 da lista de exercícios prpostos sobre Entrada de dados

---
+++ PROJETOS/5 - exercicios propostos/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio03 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um valor inteiro para A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Digite um valor inteiro para B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Digite um valor inteiro para C: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("Digite um valor inteiro para D: ");
+            int d = int.Parse(Console.ReadLine());
+
+            int resultado = (a * b) - (c * d);
+
+            Console.WriteLine("DIFERENÇA = {0}", resultado);
+        }
+    }
+}

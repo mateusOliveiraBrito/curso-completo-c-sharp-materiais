1bc44966869c45d7e26e3e499ab6ddb3171fa5bf
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 14:14:37 -0300

    :tada: Implementa primeiro exercicio da lista de exercícios propostos sobre a estrutura de repetição WHILE

---
+++ PROJETOS/11 - repeticao while/ExerciciosPropostos/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio01 {
+    class Program {
+        static void Main(string[] args) {
+            int senha = 2002;
+            bool acessoPermitido = false;
+
+            while (acessoPermitido == false) {
+                Console.Write("Digite a senha: ");
+                int senhaInserida = int.Parse(Console.ReadLine());
+
+                if (senha == senhaInserida) {
+                    Console.WriteLine("Acesso Permitido!");
+                    acessoPermitido = true;
+                } else {
+                    Console.WriteLine("Senha Inválida!");
+                }
+            }
+        }
+    }
+}

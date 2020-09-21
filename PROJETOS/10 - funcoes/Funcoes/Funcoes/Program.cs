205890b2ec6e69417366b43c24e5310834519466
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 11:52:55 -0300

    :tada: Adiciona projeto referente a aula de funções

---
+++ PROJETOS/10 - funcoes/Funcoes/Funcoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Funcoes {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite 3 números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O maior número é: " + DefineMaior(n1, n2, n3));
+        }
+
+        public static int DefineMaior(int n1, int n2, int n3) {
+            int maior = 0;
+
+            if (n1 > n2 && n1 > n3) {
+                maior = n1;
+            } else if (n2 > n3) {
+                maior = n2;
+            } else {
+                maior = n3;
+            }
+            return maior;
+        }
+    }
+}

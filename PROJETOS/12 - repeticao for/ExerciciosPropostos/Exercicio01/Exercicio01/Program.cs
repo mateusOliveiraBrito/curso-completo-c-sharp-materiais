b6fb597d2933aad186692d6e7bde8cfd7fd9cfcd
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 15:00:41 -0300

    :tada: Implementa primeiro exercicio da lista de exercicios propostos sobre estrutura de repeticao FOR

---
+++ PROJETOS/12 - repeticao for/ExerciciosPropostos/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um valor entre 1 e 1000: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n >= 1 && n <= 1000) {
+
+                for(int i = 1; i <= n; i++) {
+                    if (i % 2 != 0) {
+                        Console.WriteLine(i);
+                    }
+                }
+            } else {
+                Console.WriteLine("Valor inválido!");
+            }
+        }
+    }
+}

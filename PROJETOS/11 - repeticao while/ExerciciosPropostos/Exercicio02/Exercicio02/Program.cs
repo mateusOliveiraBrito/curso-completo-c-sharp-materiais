c9c2a07695ef4fdf30144d7940e5bedd2985c939
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 14:21:25 -0300

    ::

---
+++ PROJETOS/11 - repeticao while/ExerciciosPropostos/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio02 {
+    class Program {
+        static void Main(string[] args) {
+            int x = -1, y = -1;
+
+            while (x != 0 && y != 0) {
+                Console.Write("Digite a coordenada X: ");
+                x = int.Parse(Console.ReadLine());
+                Console.Write("Digite a coordenada Y: ");
+                y = int.Parse(Console.ReadLine());
+
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("primeiro");
+                } else if (x < 0 && y > 0) {
+                    Console.WriteLine("segundo");
+                } else if (x < 0 && y < 0) {
+                    Console.WriteLine("terceiro");
+                } else if (x > 0 && y < 0) {
+                    Console.WriteLine("quarto");
+                }
+            }
+        }
+    }
+}

10f8910f4e313af55f948ea256abbd3e22165885
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 14:48:57 -0300

    :tada: Adiciona projeto referente a aula de estrutura de repetição for;

---
+++ PROJETOS/12 - repeticao for/EstruturaFor/EstruturaFor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstruturaFor {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int qnt = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i < qnt; i++) {
+                int n = int.Parse(Console.ReadLine());
+                soma += n;
+                Console.WriteLine($"Valor #{i}: " + n);
+            }
+
+            Console.WriteLine("Soma = {0}", soma);
+        }
+    }
+}

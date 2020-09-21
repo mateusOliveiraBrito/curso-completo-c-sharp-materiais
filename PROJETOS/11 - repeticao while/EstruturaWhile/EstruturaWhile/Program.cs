64e6c8c0b40a8910a9da50b51976ff7e9d6e849a
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 14:01:49 -0300

    :tada: Adiciona projeto sobre estrutura de repetição WHILE

---
+++ PROJETOS/11 - repeticao while/EstruturaWhile/EstruturaWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstruturaWhile {
+    class Program {
+        static void Main(string[] args) {
+            int n = 0;
+
+            while(n >= 0) {
+                Console.WriteLine("Digite um número: ");
+                n = int.Parse(Console.ReadLine());
+                
+                if(n >= 0) {
+                    Console.WriteLine(Math.Sqrt(n).ToString("F3"));
+                }
+            }
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}

4da554411fae47c012b29f38bfb81f21584dfdb7
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 16:02:22 -0300

    :tada: Implementa projeto que soluciona o problema seguindo definições básicas da orientação a objetos (classes, atributos etc);

---
+++ PROJETOS/Orientação a Objetos/problema sem O.O/ProblemaComOrientacaoObj/ProblemaComOrientacaoObj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProblemaComOrientacaoObj {
+    class Program {
+        static void Main(string[] args) {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine()); 
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double p = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de X = {0}", areaX);
+            Console.WriteLine("Área de Y = {0}", areaY);
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            } else {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/problema sem O.O/ProblemaComOrientacaoObj/ProblemaComOrientacaoObj/Triangulo.cs
@@ -0,0 +1,8 @@
+﻿namespace ProblemaComOrientacaoObj {
+    class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+    }
+}

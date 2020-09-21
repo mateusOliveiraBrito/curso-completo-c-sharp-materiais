0a59d8f66f5ade60896ad38c8713380288ac17f5
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-21 15:06:20 -0300

    :tada: Implementa segundo exercicio da lista de exercicios propostos laço FOR

---
+++ PROJETOS/12 - repeticao for/ExerciciosPropostos/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio02 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int ins = 0, outs = 0;
+
+            for (int i = 0; i < n; i++) {
+                int x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20) {
+                    ins++;
+                }
+                else {
+                    outs++;
+                }
+            }
+
+            Console.WriteLine($"{ins} in");
+            Console.WriteLine($"{outs} out");
+        }
+    }
+}

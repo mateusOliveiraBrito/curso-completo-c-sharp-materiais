2f9a0741dbdb081894a6f8e821725bea88824c97
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-22 08:56:57 -0300

    :tada: Adiciona a implementação do projeto de exemplo da criação de classe e da instanciação de objetos juntamente com a criação de métodos internos da classe

---
+++ PROJETOS/Orientação a Objetos/introducao a O.O/SegundoProblemaComOO/CadastroProdutos/CadastroProdutos/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroProdutos {
+    class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/introducao a O.O/SegundoProblemaComOO/CadastroProdutos/CadastroProdutos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CadastroProdutos {
+    class Program {
+        static void Main(string[] args) {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {produto.Nome}, $ {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Dados atualizados: {produto.Nome}, 4 {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Dados atualizados: {produto.Nome}, 4 {produto.Preco}, {produto.Quantidade} unidades, Total: $ {produto.ValorTotalEmEstoque()}");
+        }
+    }
+}

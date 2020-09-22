b928bdd1f66839ae39f4b607d50903457b3b4f48
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-22 10:55:13 -0300

    :art: Altera projeto sobre membros estáticos e adiciona projeto que possui classe que funciona com base em membros estáticos

---
+++ PROJETOS/Orientação a Objetos/1-3 membros estáticos/MembrosEstaticos/MembrosEstaticos/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MembrosEstaticos {
+    class Calculadora {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double raio) {
+            return 2 * Pi * raio;
+        }
+
+        public static double Volume(double r) {
+            return 4 / 3 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-3 membros estáticos/MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"CIRCUNFERÊNCIA: {Calculadora.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture)}"); 
+            Console.WriteLine($"VOLUME: {Calculadora.Volume(raio).ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"VALOR DE PI: {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}

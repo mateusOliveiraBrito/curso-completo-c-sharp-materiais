d0e5d1c832057ce2a43f3870dc2ffa1c9a633ff0
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 11:53:44 -0300

    :tada: Adiciona projeto referente a utilização de vetores com objetos tipo referência;

---
+++ PROJETOS/Orientação a Objetos/1-8 vetores/Vetores02Objetos/Vetores02Objetos/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores02Objetos {
+    class Produto {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-8 vetores/Vetores02Objetos/Vetores02Objetos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Net.NetworkInformation;
+
+namespace Vetores02Objetos {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe a quantidade de produtos que serão inseridos: ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            Produto[] produtos = new Produto[qnt]; 
+
+            for (int i = 0; i < qnt; i++) {
+                Console.Write("Informe o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o preço do produto: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produtos[i] = new Produto(nome, preco);
+            }
+            Console.WriteLine($"Média de preços = $ {CalculaMedia(produtos).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public static double CalculaMedia(Produto[] produtos) {
+            double soma = 0.0, media = 0.0;
+            for (int i = 0; i < produtos.Length; i++) {
+                soma += produtos[i].Preco;
+            }
+
+            media = soma / produtos.Length;
+            return media;
+        }
+    }
+}

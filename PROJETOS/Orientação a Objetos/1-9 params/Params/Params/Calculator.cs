053fb44b8c5bad7f4fcd1edac83b28b7f43ec302
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 12:54:48 -0300

    :tada: Adiciona projeto que possui um exemplo referente ao uso do modificador de parâmetro 'params';

---
+++ PROJETOS/Orientação a Objetos/1-9 params/Params/Params/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Params {
+    class Calculator {
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-9 params/Params/Params/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Params {
+    class Program {
+        static void Main(string[] args) {
+            int result = Calculator.Sum(10, 20, 30, 40);
+            Console.WriteLine(result);
+        }
+    }
+}

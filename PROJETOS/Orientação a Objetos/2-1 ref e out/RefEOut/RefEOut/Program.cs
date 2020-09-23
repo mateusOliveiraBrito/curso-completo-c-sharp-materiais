f76e3772ded1f15932a3cf5058a74aca04bf2805
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 19:59:15 -0300

    :tada: Adiciona projeto com exemplo de uso dos modificadores de parâmetro ref e out;

---
+++ PROJETOS/Orientação a Objetos/2-1 ref e out/RefEOut/RefEOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RefEOut {
+    class Program {
+        static void Main(string[] args) {
+            int x = 5;
+            int y = 3;
+            int saida;
+
+            Console.WriteLine("Antes do cálculo: ");
+            Console.WriteLine($"X = {x} || Y = {y}");
+            Triplicador.TriplicaPorReferencia(ref x);
+            Triplicador.TriplicaPorSaida(y, out saida);
+
+            Console.WriteLine();
+            Console.WriteLine("Depois do cálculo: ");
+            Console.WriteLine($"X = {x} || Y = {saida}");
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/2-1 ref e out/RefEOut/RefEOut/Triplicador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefEOut {
+    class Triplicador {
+
+        public static void TriplicaPorReferencia(ref int origem) {
+            origem = origem * 3;
+        }
+
+        public static void TriplicaPorSaida(int origem, out int resultado) {
+            resultado = origem * 3;
+        }
+
+    }
+}

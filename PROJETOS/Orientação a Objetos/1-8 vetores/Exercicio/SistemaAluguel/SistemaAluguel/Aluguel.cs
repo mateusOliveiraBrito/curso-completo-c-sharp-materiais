21c68a481db148c48e982c3882a52f482163f0d4
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 12:39:56 -0300

    :tada: Adiciona projeto do exercício de fixação sobre o conteudo vetores;

---
+++ PROJETOS/Orientação a Objetos/1-8 vetores/Exercicio/SistemaAluguel/SistemaAluguel/Aluguel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaAluguel {
+    class Aluguel {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Aluguel(string nome, string email, int quarto) {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString() {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-8 vetores/Exercicio/SistemaAluguel/SistemaAluguel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SistemaAluguel {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluguel[] alugueis = Adiciona(LeituraInicial());
+            ListaAlugueis(alugueis);
+        }
+
+        public static int LeituraInicial() {
+            Console.Write("Quantos quartos serão alugados? ");
+            int qnt = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            return qnt;
+        }
+
+        public static Aluguel[] Adiciona(int qnt) {
+            Aluguel[] alugueis = new Aluguel[qnt];
+
+            for (int i = 0; i < qnt; i++) {
+                Console.WriteLine($"Alguel #{i + 1}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Aluguel aluguel = new Aluguel(nome, email, quarto);
+
+                alugueis[i] = aluguel;
+            }
+
+            return alugueis;
+        }
+
+        public static void ListaAlugueis(Aluguel[] alugueis) {
+            Console.WriteLine("Quartos ocupados: ");
+            Aluguel[] quartosOcupados = new Aluguel[10];
+
+            for (int i = 0; i < alugueis.Length; i++) {
+                quartosOcupados[alugueis[i].Quarto] = alugueis[i];
+            }
+
+            for (int i = 0; i < quartosOcupados.Length; i++) {
+                if (quartosOcupados[i] != null) {
+                    Console.WriteLine(quartosOcupados[i]);
+                }
+            }
+        }
+    }
+}

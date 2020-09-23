03930eb98bb2044e28674c6cc5862864f15fa52f
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 08:56:22 -0300

    :tada: Implementa projeto 'Conta Bancária' referente à aula de exercício de fixação sobre orientação a objetos em C#;

---
+++ PROJETOS/Orientação a Objetos/1-5 exercicios propostos sobre OO/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ContaBancaria {
+    class Conta {
+        /* o modificador de acesso private antes do set significa que ele nao poderá ser alterado
+         nesse caso de Numero, se dentro da classe ContaBancaria houvesse um método para alterar esse valor, ele seria alterado,
+         no entanto não existe, por isso ele não poderá ser alterado em outras classes do programa*/
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        /* o modificador de acesso private indica que Saldo não poderá ser acessado por outra classe e que suas modificações
+         só poderão ocorrer através dos métodos de saque e depósito. */
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome) {
+            this.Numero = numero;
+            this.Titular = nome;
+            this.Saldo = 0.0;
+        }
+
+        public Conta(int numero, string nome, double saldoInicial) : this(numero, nome) {
+            this.Saldo = saldoInicial;
+        }
+
+        public void Deposito(double valor) {
+            this.Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            this.Saldo -= (valor + 5);
+        }
+
+        public override string ToString() {
+            return $"Conta: {this.Numero}, Titular: {this.Titular}, Saldo: $ {this.Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-5 exercicios propostos sobre OO/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria {
+    class Program {
+        static void Main(string[] args) {
+            int numero;
+            string titular;
+            char opcao;
+            double valor;
+            Conta conta = null;
+
+            Console.Write("Entre com o número da conta: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial: (s/n): ");
+            opcao = char.Parse(Console.ReadLine());
+
+            if (opcao == 's') {
+                Console.Write("Entre com o depósito inicial: ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, valor);
+                Console.WriteLine("");
+            }
+            else if (opcao == 'n'){
+                conta = new Conta(numero, titular);
+                Console.WriteLine("");
+            }
+            else {
+                Console.WriteLine("Opção inválida");
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+            Console.WriteLine("");
+
+            Console.Write("Entre com um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valor);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+            Console.WriteLine("");
+
+            Console.Write("Entre com um valor para o saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valor);
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}

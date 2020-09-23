1081d592ee7a14b98091534bed006b225288ee35
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-23 10:30:23 -0300

    :tada: Implementa projeto referente ao conteúdo de tipos referência e tipos valor;

---
+++ PROJETOS/Orientação a Objetos/1-6 tipos referência e tipos valor/Struct/Struct/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct {
+    struct Point {
+        public double X;
+        public double Y;
+
+        public override string ToString() {
+            return $"({X},{Y})";
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/1-6 tipos referência e tipos valor/Struct/Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Struct {
+    class Program {
+        static void Main(string[] args) {
+            //foram atribuídos os valores para X e Y para que o compilador entenda que o struct (variável) foi inicializado
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            /*
+             Se depois de definir valores para X e Y fazer uma instância do objeto, os valores serão recriados 
+             e irão assumir o valor zero
+             Exemplo: p = new Point();
+             */
+
+            /*
+             * CONCLUSÃO: mesmo sendo struct, deve-se inicializar, seja definindo valores ou instanciando o objeto
+             */
+        }
+    }
+}

1ebba53a2e177c2f8208ae4fc1685e0c23435c37
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-09-24 08:29:12 -0300

    :tada: Adiciona projeto com exemplo de uso do foreach

---
+++ PROJETOS/Orientação a Objetos/2-2 foreach/Foreach/Foreach/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args) {
+            string[] nomes = { "mateus", "oliveira", "brito" };
+
+            foreach(string nome in nomes) {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}

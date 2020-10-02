0c32fdda52ac9f1f3b9911584d85078e8ae3a698
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-02 12:19:57 -0300

    :tada: Adiciona projeto do exercício de fixação sobre listas (List);

---
+++ PROJETOS/Orientação a Objetos/2-3 listas/Exercicio/ProjetoListas/ProjetoListas/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ProjetoListas
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Salario = salario;
+        }
+
+        public void AumentaSalario(double porcentagem)
+        {
+            this.Salario += this.Salario * (porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, R$ {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PROJETOS/Orientação a Objetos/2-3 listas/Exercicio/ProjetoListas/ProjetoListas/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ProjetoListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            int id = 0;
+            string nome;
+            double salario;
+
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Funcionário #{i}");
+                Console.Write("Digite o id do funcionário: ");
+                id = int.Parse(Console.ReadLine());
+
+                if (VerificaFuncionarioExistente(id, funcionarios))
+                {
+                    Console.WriteLine("Um funcionário com esse Id já foi inserido, tente novamente!");
+                    Console.WriteLine();
+                    i--;
+                    continue;
+                }
+                else
+                {
+                    Console.Write("Digite o nome do funcionário: ");
+                    nome = Console.ReadLine();
+                    Console.Write("Digite o salário do funcionário: ");
+                    salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    funcionarios.Add(new Funcionario(id, nome, salario));
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Write("Insira o Id do funcionário que terá aumento salarial: ");
+            id = int.Parse(Console.ReadLine());
+
+            if (VerificaFuncionarioExistente(id, funcionarios))
+            {
+                Console.Write("Digite a porcentagem do aumento salarial: ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                Funcionario funcionario = funcionarios.Find(x => x.Id == id);
+                funcionario.AumentaSalario(porcentagem);
+
+                Console.WriteLine();
+                Console.WriteLine("Lista de funcionários atualizada:");
+
+                ListaFuncionarios(funcionarios);
+            } else
+            {
+                Console.WriteLine("Esse funcionário não existe!");
+                Console.WriteLine();
+            }
+        }
+
+        public static bool VerificaFuncionarioExistente(int id, List<Funcionario> funcionarios)
+        {
+            Funcionario funcionarioExistente = funcionarios.Find(x => x.Id == id);
+            if (funcionarioExistente != null)
+                return true;
+            else
+                return false;
+        }
+
+        public static void ListaFuncionarios(List<Funcionario> funcionarios)
+        {
+            foreach(var item in funcionarios)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

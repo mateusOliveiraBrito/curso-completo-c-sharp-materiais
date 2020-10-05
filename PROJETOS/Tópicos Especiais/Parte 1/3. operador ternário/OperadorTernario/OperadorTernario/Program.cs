1156868f291849b902b797d54f4340fabf9eb54f
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-05 12:12:33 -0300

    :tada: Implementa projeto sobre operador ternário;

---
+++ PROJETOS/Tópicos Especiais/Parte 1/3. operador ternário/OperadorTernario/OperadorTernario/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            double desconto;
+
+            desconto = (preco < 20) ? (preco * 0.1) : (preco * 0.05);
+
+            Console.WriteLine($"Desconto: {desconto}");
+        }
+    }
+}

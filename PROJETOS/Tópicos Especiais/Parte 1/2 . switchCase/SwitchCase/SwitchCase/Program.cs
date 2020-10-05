758627e5c54472b0970c39ee3be2888ffdac3dee
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-05 12:05:35 -0300

    :tada: Implementa projeto estrutura switch-case;

---
+++ PROJETOS/Tópicos Especiais/Parte 1/2 . switchCase/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o dia da semana usando digitos de 1 a 7: ");
+            int n = int.Parse(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Opção inexistente");
+                    break;
+            }
+        }
+    }
+}

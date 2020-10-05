4743735d1c1b8e3ce94a9850e339122cad8b4e3c
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-05 11:59:25 -0300

    :tada: Implementa projeto referente à inferência de tipos com tipo var;

---
+++ PROJETOS/Tópicos Especiais/Parte 1/1. var/Var/Var/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inferência de tipos: palavra var
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}

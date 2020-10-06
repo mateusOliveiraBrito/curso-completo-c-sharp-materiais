cac9b9e59fe8b4c7e12d56d4059fbfc08573f54d
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-06 09:20:52 -0300

    :tada: Implementa projeto que utiliza timespan e projeto que utiliza operacao com datetime;

---
+++ PROJETOS/Tópicos Especiais/Parte 1/6. timespan/TimeSpanPrj/TimeSpanPrj/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TimeSpanPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(1, 30, 15); //hh mm ss
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21); //dd hh mm ss
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321); //dd hh mm ss ms
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            TimeSpan time1 = TimeSpan.FromDays(1.5);
+            TimeSpan time2 = TimeSpan.FromHours(1.5);
+            TimeSpan time3 = TimeSpan.FromMinutes(1.5);
+            TimeSpan time4 = TimeSpan.FromSeconds(1.5);
+            TimeSpan time5 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan time6 = TimeSpan.FromTicks(900000000L); //só aceita valores inteiros por ser a menor medida
+
+            Console.WriteLine(time1);
+            Console.WriteLine(time2);
+            Console.WriteLine(time3);
+            Console.WriteLine(time4);
+            Console.WriteLine(time5);
+            Console.WriteLine(time6);
+        }
+    }
+}
---
+++ PROJETOS/Tópicos Especiais/Parte 1/7. operacaoDateTime/OpDateTime/OpDateTime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OpDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Data: " + d.Date);
+            Console.WriteLine("2) Dia: " + d.Day);
+            Console.WriteLine("3) Dia da semana: " + d.DayOfWeek);
+            Console.WriteLine("4) Dia do ano: " + d.DayOfYear);
+            Console.WriteLine("5) Hora: " + d.Hour);
+            Console.WriteLine("6) Tipo (local/global): " + d.Kind); //por padrão é local, mas ele nao especifica se o programador não explicitar isso em código
+            Console.WriteLine("7) Mili-segundo: " + d.Millisecond);
+            Console.WriteLine("8) Minuto: " + d.Minute);
+            Console.WriteLine("9) Mês: " + d.Month);
+            Console.WriteLine("10) Segundo: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) Horário do dia: " + d.TimeOfDay);
+            Console.WriteLine("13) Ano: " + d.Year);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //trabalhando com formatação
+            DateTime d2 = new DateTime(2001, 8, 15, 13, 45, 58);
+
+            Console.WriteLine(d2.ToLongDateString()); //converte a data para string
+            Console.WriteLine(d2.ToLongTimeString());
+            Console.WriteLine(d2.ToShortDateString());
+            Console.WriteLine(d2.ToShortTimeString());
+            Console.WriteLine(d2.ToString());
+
+            string s = d2.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine(s);
+
+            string s2 = d2.ToString("yyyy-MM-dd HH:mm:ss.ff");
+            Console.WriteLine(s2);
+
+            //operações com dateTime
+            DateTime dOp = d.AddHours(2);
+            Console.WriteLine(dOp.Hour);
+        }
+    }
+}

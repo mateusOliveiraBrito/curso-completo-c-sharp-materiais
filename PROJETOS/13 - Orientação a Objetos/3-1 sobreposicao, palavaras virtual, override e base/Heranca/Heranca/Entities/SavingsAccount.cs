39b60ec7766e509f12d59d6f1746698d63feb7af
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-07 12:18:54 -0300

    :tada: Implementa projeto com exemplos de sobreposição, usando palavra virtuaç, override e base;

---
+++ PROJETOS/13 - Orientação a Objetos/3-1 sobreposicao, palavaras virtual, override e base/Heranca/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+        public SavingsAccount()
+        { }
+
+        public SavingsAccount(int number, string holder, double balance, double InterestRate) 
+            : base(number, holder, balance)
+        {
+            InterestRate = InterestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //a palavra base serve para chamar a implementação da super classe, para casos em que deseja-se reutilizar 
+        //o código e adicionar mais alguma função nele.
+        public override void WithDraw(double amount)
+        {
+            base.WithDraw(amount);
+            Balance -= 2;
+        }
+    }
+}

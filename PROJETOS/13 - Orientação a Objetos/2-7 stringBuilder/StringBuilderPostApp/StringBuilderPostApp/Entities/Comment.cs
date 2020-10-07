71d8db3bb6b56b7c34c081c83c4713a0b9634316
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-07 09:23:21 -0300

    :tada: Implementa projeto seguindo composicao de objetos e utilizacao de stringbuilder;

---
+++ PROJETOS/13 - Orientação a Objetos/2-7 stringBuilder/StringBuilderPostApp/StringBuilderPostApp/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace StringBuilderPostApp.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/2-7 stringBuilder/StringBuilderPostApp/StringBuilderPostApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using StringBuilderPostApp.Entities;
+using System;
+
+namespace StringBuilderPostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment1 = new Comment("Have a nice trip!");
+            Comment comment2 = new Comment("Wow that' awesome!");
+
+            Post post1 = new Post() {
+                Moment = DateTime.Parse("21/06/2020 13:05:44"),
+                Title = "Traveling to New Zealand",
+                Content = "I'm going to visit this wonderful country!",
+                Likes = 12
+            };
+
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+            Console.WriteLine(post1);
+
+            Comment comment3 = new Comment("Good night");
+            Comment comment4 = new Comment("May the force be with you");
+
+            Post post2 = new Post()
+            {
+                Moment = DateTime.Parse("28/07/2020 23:14:19"),
+                Title = "Good night guys",
+                Content = "See you tomorrow",
+                Likes = 5
+            };
+
+            post2.AddComment(comment3);
+            post2.AddComment(comment4);
+
+            Console.WriteLine(post2);
+        }
+    }
+}

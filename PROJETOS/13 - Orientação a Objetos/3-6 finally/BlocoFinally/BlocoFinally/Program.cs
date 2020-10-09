62e32db91051a7f06fb798124d0557a240900160
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-09 09:14:08 -0300

    :tada: Implementa projeto utilizando bloco finnaly;

---
+++ PROJETOS/13 - Orientação a Objetos/3-6 finally/BlocoFinally/BlocoFinally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoFinally
+{
+    class Program
+    {
+        public static void Main()
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}

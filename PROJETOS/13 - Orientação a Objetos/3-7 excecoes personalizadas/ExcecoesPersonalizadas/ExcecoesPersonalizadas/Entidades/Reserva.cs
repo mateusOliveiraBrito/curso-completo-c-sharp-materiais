e3f3e11db4d555f7356f38b4c8488b583e604079
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-09 09:42:01 -0300

    :tada: Implementa projeto de Reservas sem utilização de tratamento de exceções e sem delegar corretamente as funções do programa;

---
+++ PROJETOS/13 - Orientação a Objetos/3-7 excecoes personalizadas/ExcecoesPersonalizadas/ExcecoesPersonalizadas/Entidades/Reserva.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExcecoesPersonalizadas.Entidades
+{
+    class Reserva
+    {
+        public int NumeroQuarto { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int numeroQuarto, DateTime checkin, DateTime checkout)
+        {
+            NumeroQuarto = numeroQuarto;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duracao = Checkout.Subtract(Checkin);
+            return (int)duracao.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"Quarto {NumeroQuarto}, check-in: {Checkin.ToString("dd/MM/yyyy")}, check-out: {Checkout.ToString("dd/MM/yyyy")}, {Duracao()} noites";
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/3-7 excecoes personalizadas/ExcecoesPersonalizadas/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,53 @@
+﻿using ExcecoesPersonalizadas.Entidades;
+using System;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Número do quarto: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Check-in (DD/MM/AAAA): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out (DD/MM/AAAA): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if (checkout <= checkin)
+            {
+                Console.WriteLine("Erro na reserva: Datas cronológicamente incompatíveis!");
+            }
+            else
+            {
+                Reserva reserva = new Reserva(numero, checkin, checkout);
+                Console.WriteLine($"Reserva: {reserva}");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Entre com os dados para atualizar a reserva:");
+                Console.Write("Check-in (DD/MM/AAAA): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out (DD/MM/AAAA): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime agora = DateTime.Now;
+
+                if (checkin < agora || checkout < agora)
+                {
+                    Console.WriteLine("Erro na reserva: data para atualização de reserva, devem ser datas futuras!");
+                }
+                else if (checkout <= checkin)
+                {
+                    Console.WriteLine("Erro na reserva: Datas cronológicamente incompatíveis!");
+                }
+                else
+                {
+                    reserva.UpdateDates(checkin, checkout);
+                    Console.WriteLine($"Reserva: {reserva}");
+                }
+            }
+        }
+    }
+}

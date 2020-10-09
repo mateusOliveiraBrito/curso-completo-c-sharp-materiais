dcf25b7b6a06351ca2f6bb5c1667afc4ceef0e4c
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-09 09:08:30 -0300

    :tada: Implementa projeto utilizando estrutura try/catch para tratamento de exceção;

---
+++ PROJETOS/13 - Orientação a Objetos/3-5 try catch/TryCatch/TryCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //stacktrace (rastreamento das chamadas das exceções) = mensagem que descreve o tipo da exceção, 
+            //mensagem padrao da exceção e onde ocorreu a exceção
+
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+                Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é permitido divisão por zero!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Não é permitido inserir valores não numéricos!");
+            }
+        }
+    }
+}

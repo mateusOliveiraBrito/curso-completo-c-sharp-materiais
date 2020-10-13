28bf172b68a2071335481707354003612fc5dd4b
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-13 12:15:14 -0300

    :art: Trata possíveis erros utilizando exceções personalizadas;

---
+++ PROJETOS/13 - Orientação a Objetos/3-8 sistema jogo de xadrez/xadrez-console/xadrez-console/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string mensagem) : base(mensagem)
+        {
+            
+        }
+    }
+}

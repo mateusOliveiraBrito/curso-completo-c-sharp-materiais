a8fabcac650294553afd32117b5ad429adbfe98b
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-19 11:02:16 -0300

    :tada: Implementa projeto utilizando recursos de stream para manipular arquivos, à grosso modo;

---
+++ PROJETOS/13 - Orientação a Objetos/3-9 arquivos/Streader/Streaderprj/ProjStreamReader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ProjStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string linha = sr.ReadLine();
+                Console.WriteLine(linha);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+    }
+}

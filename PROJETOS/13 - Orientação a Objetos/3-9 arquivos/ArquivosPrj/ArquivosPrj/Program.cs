dea065338797d88c1e46866578872d95fb41975c
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-19 09:59:48 -0300

    :tada: Implementa projeto de introdução à manipulação de arquivos;

---
+++ PROJETOS/13 - Orientação a Objetos/3-9 arquivos/ArquivosPrj/ArquivosPrj/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ArquivosPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //caminho do arquivo de origem
+            //a notação @ serve para que não seja necessário digitar \\ no sourcePath
+            string sourcePath = @"c:\temp\file1.txt";
+
+            //caminho do arquivo de destino
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                //instancia um objeto com base num arquivo existente
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                //copia os dados do objeto para um arquivo em um caminho especificado
+                fileInfo.CopyTo(targetPath);
+
+                string[] linhas = File.ReadAllLines(sourcePath);
+
+                foreach(var item in linhas)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"ERRO:{e.Message}");
+            }
+        }
+    }
+}

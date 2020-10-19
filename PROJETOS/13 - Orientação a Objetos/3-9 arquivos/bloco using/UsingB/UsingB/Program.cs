b81358624896f6458faf40f4a47d760a28eadc57
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-10-19 11:20:47 -0300

    :tada: Implementa projeto utilizando bloco using;

---
+++ PROJETOS/13 - Orientação a Objetos/3-9 arquivos/bloco using/UsingB/UsingB/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace UsingB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            /*
+             * dentro dos parênteses do bloco using são instanciados
+             * os objetos que deverão ser fechados
+            */
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string linha = sr.ReadLine();
+                            Console.WriteLine(linha);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"ERRO: {e.Message}");
+            }
+        }
+    }
+}

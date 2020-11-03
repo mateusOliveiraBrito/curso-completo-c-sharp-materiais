0d1a86ea2d2f32405e345fad52562edd3c96851d
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-03 11:25:55 -0300

    :tada: Implementa Projeto utilizando Directory e DirectoryInfo, para visualização da estrutura de diretórios do projeto no programa;

---
+++ PROJETOS/13 - Orientação a Objetos/3-9 arquivos/DirectoryDirecInfo/DirectoryDirecInfo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryDirecInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myFolder";
+
+            try
+            {
+                /*
+                 * LISTAGEM DE PASTAS:
+                 * Sobrecarga desse método: caminho base, padrão de busca (máscara de busca) na forma de string 
+                 * e opção de busca
+                 * 
+                 * IEnumerable é um tipo de coleção ainda mais genérico que o tipo List
+                 */
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+
+                foreach(var item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+
+                /*
+                 * LISTAGEM DE ARQUIVOS:
+                 * Código semelhante ao de listagem de pastas
+                 */
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+
+                foreach(var item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                /*
+                 * CRIANDO PASTA
+                 */
+                Directory.CreateDirectory(path + @"\newFolder");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO: " + ex.Message);
+            }
+        }
+    }
+}

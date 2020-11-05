191774876b9b5d82e1ffd5d377c3c1502d04b003
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-05 10:32:34 -0300

    :tada: Implementa projeto que utiliza IComparable;

---
+++ PROJETOS/13 - Orientação a Objetos/4-1 interfaces/IComparable/CompararObjeto/CompararObjeto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CompararObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

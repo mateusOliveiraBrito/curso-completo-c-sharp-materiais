33589b8c559fc6ddaa5aab0d5445337b29a5c09f
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-06 12:23:03 -0300

    :tada: Implementa projeto exemplificando o uso básico de um conjunto do tipo HashSet;

---
+++ PROJETOS/13 - Orientação a Objetos/4-4 hashSet e sortedSet/exemplo1/exemplo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo com HashSet
+            HashSet<string> conjuntoHSet = new HashSet<string>();
+
+            conjuntoHSet.Add("TV");
+            conjuntoHSet.Add("Notebook");
+            conjuntoHSet.Add("Tablet");
+            conjuntoHSet.Add("Smartphone");
+
+            // Verifica se no conjunto existe o objeto do parâmetro
+            Console.WriteLine(conjuntoHSet.Contains("Notebook"));
+
+            // Utiliza-se o foreach porque não é possível utilizar o for, pois os elementos de um conjunto ão têm posição
+            foreach (string item in conjuntoHSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

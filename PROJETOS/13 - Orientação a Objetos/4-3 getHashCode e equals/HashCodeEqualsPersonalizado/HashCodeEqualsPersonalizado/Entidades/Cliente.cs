3e48d450bfe5816b0ce95d15ea0a703eb563d2ca
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-06 10:42:15 -0300

    :tada: Implementa projeto sobrescrevendo métodos Equals e GetHashCode;

---
+++ PROJETOS/13 - Orientação a Objetos/4-3 getHashCode e equals/HashCodeEqualsPersonalizado/HashCodeEqualsPersonalizado/Entidades/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCodeEqualsPersonalizado.Entidades
+{
+    class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Cliente))
+                return false;
+
+            Cliente newCliente = obj as Cliente;
+            return Email.Equals(newCliente.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-3 getHashCode e equals/HashCodeEqualsPersonalizado/HashCodeEqualsPersonalizado/Program.cs
@@ -0,0 +1,37 @@
+﻿using HashCodeEqualsPersonalizado.Entidades;
+using System;
+
+namespace HashCodeEqualsPersonalizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente a = new Cliente("Mateus", "m@email.com");
+            Cliente a2 = new Cliente("Mateus", "m@email.com");
+            Cliente b = new Cliente("Altair", "a@email.com");
+
+            Console.WriteLine(a.Equals(a2));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            if (a.GetHashCode() == b.GetHashCode())
+            {
+                Console.WriteLine("IGUAIS");
+            }
+            else
+            {
+                Console.WriteLine("DIFERENTES");
+            }
+
+            if (a.GetHashCode() == a2.GetHashCode())
+            {
+                Console.WriteLine("IGUAIS");
+            }
+            else
+            {
+                Console.WriteLine("DIFERENTES");
+            }
+        }
+    }
+}

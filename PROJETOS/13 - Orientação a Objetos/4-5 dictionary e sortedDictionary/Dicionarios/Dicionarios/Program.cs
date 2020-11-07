cbd388be767e0f3c5603435faf815e256e642f7c
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-07 19:40:25 -0300

    :tada: Implementa exercicio proposto sobre Dictionary;

---
+++ PROJETOS/13 - Orientação a Objetos/4-5 dictionary e sortedDictionary/Dicionarios/Dicionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dicionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria.com";
+            cookies["fone"] = "40028922";
+            cookies["fone"] = "993964985"; // por serem a mesma chave, a segunda atribuição sobrescreve a primeira
+
+            Console.WriteLine(cookies["email"]);
+
+            try
+            {
+                cookies.Remove("email");
+                Console.WriteLine(cookies["email"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Chave inexistente");
+            }
+
+            Console.WriteLine("Tamanho: " + cookies.Count);
+            Console.WriteLine("TODOS OS COOKIES: ");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-5 dictionary e sortedDictionary/exerc/Votos/Votos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Votos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> registros = new Dictionary<string, int>();
+
+            string path = @"c:\temp\votos\registros.csv";
+
+            using (StreamReader str = File.OpenText(path))
+            {
+                while (!str.EndOfStream)
+                {
+                    string[] linha = str.ReadLine().Split(',');
+
+                    if (registros.ContainsKey(linha[0]))
+                    {
+                        registros[linha[0]] += int.Parse(linha[1]);
+                    }
+                    else
+                    {
+                        //registros[linha[0]] = int.Parse(linha[1]);
+                        registros.Add(linha[0], int.Parse(linha[1]));
+                    }
+                }
+
+                foreach (var item in registros)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}

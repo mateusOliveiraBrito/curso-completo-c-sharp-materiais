bb9a530325dbbc90560e61b9eddd5ba6df6aa8f7
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-07 18:56:38 -0300

    :tada: Implementa projeto de Exercício proposto sobre conjuntos;

---
+++ PROJETOS/13 - Orientação a Objetos/4-4 hashSet e sortedSet/exercicio/Alunos/Alunos/Entidades/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Alunos.Entidades
+{
+    class Aluno
+    {
+        public int Codigo { get; private set; }
+
+        public Aluno(int codigo)
+        {
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return Codigo.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Aluno))
+                throw new ArgumentException("Erro: O objeto informado não é um aluno");
+
+            Aluno aluno = obj as Aluno;
+
+            if (Codigo != aluno.Codigo)
+                return false;
+            else
+                return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return Codigo.GetHashCode();
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-4 hashSet e sortedSet/exercicio/Alunos/Alunos/Program.cs
@@ -0,0 +1,59 @@
+﻿using Alunos.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Alunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Aluno> A = new HashSet<Aluno>();
+            HashSet<Aluno> B = new HashSet<Aluno>();
+            HashSet<Aluno> C = new HashSet<Aluno>();
+
+            List<HashSet<Aluno>> cursos = new List<HashSet<Aluno>>();
+
+            int n;
+
+            Console.Write("Quantos alunos para o curso A? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                A.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            cursos.Add(A);
+
+            Console.Write("Quantos alunos para o curso B? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                B.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            cursos.Add(B);
+
+            Console.Write("Quantos alunos para o curso C? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                C.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            cursos.Add(C);
+
+            Console.WriteLine();
+            Console.WriteLine("Total de alunos: " + TotalAlunos(cursos));
+        }
+
+        public static int TotalAlunos(List<HashSet<Aluno>> cursos)
+        {
+            HashSet<Aluno> totalAlunos = new HashSet<Aluno>(cursos[0]);
+            totalAlunos.UnionWith(cursos[1]);
+            totalAlunos.UnionWith(cursos[2]);
+
+            return totalAlunos.Count;
+        }
+    }
+}

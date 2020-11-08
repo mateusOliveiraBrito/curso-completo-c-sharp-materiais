444779f0b93691615ab23ba53ec5c94f338e0c05
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 12:17:58 -0300

    :tada: Implementa método usando o Delegate Func;

---
+++ PROJETOS/13 - Orientação a Objetos/4-9 func/FuncEx/FuncEx/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FuncEx.Entities
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Product(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return $@"{Nome}, {Preco.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-9 func/FuncEx/FuncEx/Program.cs
@@ -0,0 +1,37 @@
+﻿using FuncEx.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace FuncEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+
+            Func<Product, string> func = NameUpper; //aqui pode receber funcao 
+            List<string> result = list.Select(p => { return p.Nome.ToUpper(); }).ToList();
+            //List<string> result = list.Select(func).ToList();
+            //List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        public static string NameUpper(Product p)
+        {
+            return p.Nome.ToUpper();
+        }
+    }
+}

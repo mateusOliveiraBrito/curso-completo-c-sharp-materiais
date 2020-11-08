a22afc4ad228679e36803e7dc8879c25050bf031
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 20:25:00 -0300

    :tada: Implementa exercicio resolvido sobre consultas LINQ;

---
+++ PROJETOS/13 - Orientação a Objetos/5-2 linqSQL/exResolvido/exercicio/exercicio/Entidades/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace exercicio.Entidades
+{
+    class Produto
+    {
+        public string Nome { get; private set; }
+        public double Preco { get; private set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return $@"{Nome}, {Preco}";
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/5-2 linqSQL/exResolvido/exercicio/exercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿using exercicio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Globalization;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\produtos.csv";
+            List<Produto> produtos = new List<Produto>();
+
+            using (StreamReader str = File.OpenText(path))
+            {
+                while (!str.EndOfStream)
+                {
+                    var linha = str.ReadLine().Split(',');
+                    produtos.Add(new Produto(linha[0], double.Parse(linha[1])));
+                }
+
+                //preço médio
+                var media = (from p in produtos select p).Select(x => x.Preco).Average();
+                Console.WriteLine("Média dos preços dos produtos: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine();
+
+                var nomes = (from p in produtos where p.Preco < media select p).OrderByDescending(x => x.Nome).Select(x => x.Nome).ToList();
+
+                Console.WriteLine("NOMES:");
+                foreach (var item in nomes)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}

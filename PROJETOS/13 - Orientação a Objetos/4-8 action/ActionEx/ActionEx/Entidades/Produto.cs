02ce7e01dab88dbdfe4c79982f67d099b82991e2
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 11:39:35 -0300

    :tada: Implementa projeto com exemplo de Action;

---
+++ PROJETOS/13 - Orientação a Objetos/4-8 action/ActionEx/ActionEx/Entidades/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ActionEx.Entidades
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return $@"{Nome}, {Preco.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-8 action/ActionEx/ActionEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using ActionEx.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace ActionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> list = new List<Produto>();
+
+            list.Add(new Produto("Tv", 900.00));
+            list.Add(new Produto("Mouse", 50.00));
+            list.Add(new Produto("Tablet", 350.50));
+            list.Add(new Produto("HD Case", 80.90));
+
+            //list.ForEach(p => p.Preco += p.Preco * 0.1);
+            //list.ForEach(p => { p.Preco += p.Preco * 0.1; });
+            list.ForEach(MudaPreco); //a função informada no parametro do método foreach do list deve retornar void e possuir um objeto por parametro do mesmo tipo da lista
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void MudaPreco(Produto p)
+        {
+            p.Preco += p.Preco * 0.1;
+        }
+    }
+}

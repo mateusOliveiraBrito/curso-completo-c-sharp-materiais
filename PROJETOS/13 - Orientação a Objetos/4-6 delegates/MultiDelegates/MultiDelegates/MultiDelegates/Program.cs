03558ec458d7f16396fe3e7509f06c469cbcda8b
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 11:12:36 -0300

    :tada: Implementa projeto sobre MultiDelegates;

---
+++ PROJETOS/13 - Orientação a Objetos/4-6 delegates/MultiDelegates/MultiDelegates/MultiDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using MultiDelegates.Services;
+using System;
+
+namespace MultiDelegates
+{
+
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-6 delegates/MultiDelegates/MultiDelegates/MultiDelegates/Services/CalculationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiDelegates.Services
+{
+    class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}

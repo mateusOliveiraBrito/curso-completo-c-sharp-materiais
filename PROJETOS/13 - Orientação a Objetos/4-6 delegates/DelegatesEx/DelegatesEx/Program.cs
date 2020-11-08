276ed92f5ca2e83e52e6ee033a13082f47bbe662
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 10:59:11 -0300

    :tada: Implementa projeto utilizando delegates;

---
+++ PROJETOS/13 - Orientação a Objetos/4-6 delegates/DelegatesEx/DelegatesEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using DelegatesEx.Services;
+using System;
+
+namespace DelegatesEx
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10, b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+            //double result = op(a, b);
+            double result = op.Invoke(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/4-6 delegates/DelegatesEx/DelegatesEx/Services/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesEx.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}

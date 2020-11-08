fe4b239cb3b9059d927590b21fbf93594dddb1df
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 19:34:37 -0300

    :tada: Implementa projeto que ensina como realizar as consultas usando sintaxe similar ao SQL;

---
+++ PROJETOS/13 - Orientação a Objetos/5-2 linqSQL/LinqLambda/LinqLambda/Entidades/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqLambda.Entidades
+{
+    class Categoria
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public int Nota { get; private set; }
+
+        public Categoria(int id, string nome, int nota)
+        {
+            Id = id;
+            Nome = nome;
+            Nota = nota;
+        }
+
+        public override string ToString()
+        {
+            return $@"{Id}, {Nome}, {Nota}";
+        }
+    }
+}
---
+++ PROJETOS/13 - Orientação a Objetos/5-2 linqSQL/LinqLambda/LinqLambda/Entidades/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LinqLambda.Entidades
+{
+    class Produto
+    {
+        public int Id { get;  set; }
+        public string Nome { get;  set; }
+        public double Preco { get;  set; }
+        public Categoria Categoria { get;  set; }
+
+        public Produto(int id, string nome, double preco, Categoria categoria)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+
+        public override string ToString()
+        {
+            return $@"{Id}, {Nome}, {Preco.ToString("F2", CultureInfo.InvariantCulture)}, {Categoria}";
+        }
+    }
+}

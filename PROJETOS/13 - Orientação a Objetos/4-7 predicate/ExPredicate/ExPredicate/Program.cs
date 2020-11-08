8146e028b9849ca2bfad038eaff7bc8da46e7fb6
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2020-11-08 11:24:17 -0300

    :tada: Implementa projeto sobre Predicates;

---
+++ PROJETOS/13 - Orientação a Objetos/4-7 predicate/ExPredicate/ExPredicate/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExPredicate.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace ExPredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100;
+        }
+    }
+}
